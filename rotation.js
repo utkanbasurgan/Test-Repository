2bedb30bf5fc14f04ca6eae279b2b633d207eca4
Utkan Başurgan <140505686+utkanbasurgan@users.noreply.github.com>
2024-09-01 10:11:17 +0300

    Create rotation.js

---
+++ rotation.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} n
+ * @param {number} time
+ * @return {number}
+ */
+var passThePillow = function(n, time)
+{
+    let start = 1;
+    let end = n;
+    let rotation = "right";
+    let current = 1;
+
+    for (i=0; i<time; i++)
+    {
+        if (rotation == "right")
+        {
+            current = current + 1;
+            console.log(current);
+        }
+        if (rotation == "left")
+        {
+            current = current - 1;
+        }
+        if (current == end)
+        {
+            rotation = "left";
+        }
+        if (current == start)
+        {
+            rotation = "right";
+        }
+    }
+
+    return current;
+};
